564a79c8ff2a02c08a2691aa84973697a4d9b6da
sean15535 <sean15535@gmail.com>
2024-03-25 09:24:40 +0100

    added functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 def button_1_click_response():
     button_click_response('1')
 
@@ -11,9 +12,26 @@ def button_3_click_response():
 def button_4_click_response():
     button_click_response('4')
 
+def button_5_click_response():
+    button_click_response('5')
+
+def button_6_click_response():
+    button_click_response('6')
+
+def button_7_click_response():
+    button_click_response('7')
+
+def button_8_click_response():
+    button_click_response('8')
+
+def button_9_click_response():
+    button_click_response('9')
+
+def button_4_click_response():
+    button_click_response('4')
+
 def button_click_response(char):
     figures.insert(END, char)
-
 
 window = Tk()
 window.minsize(width=500, height=500)
@@ -30,5 +48,11 @@ button_2.pack()
 
 button_3 = Button(window, text="3", command=button_3_click_response)
 button_3.pack()
+
+button_4 = Button(window, text="4", command=button_4_click_response)
+button_4.pack()
+
+button_5 = Button(window, text="5", command=button_5_click_response)
+button_5.pack()
 
 window.mainloop()
